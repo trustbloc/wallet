09942e2938c96b497f3cac8e0c1f60a00a2b4490
Firas Qutishat <firas.qutishat@securekey.com>
2020-01-16 12:22:36 -0500

    chore: Fix tls cert
    closes #28
    
    Signed-off-by: Firas Qutishat <firas.qutishat@securekey.com>

--- scripts/generate_test_keys.sh
+++ scripts/generate_test_keys.sh
@@ -8,11 +8,18 @@
 set -e
 
 
-echo -e  "[SAN]\nsubjectAltName=DNS:*.example.com,DNS:localhost" >> /etc/ssl/openssl.cnf
-
 echo "Generating edge-agent Test PKI"
+
 cd /opt/workspace/edge-agent
 mkdir -p test/bdd/fixtures/keys/tls
+tmp=$(mktemp)
+echo "subjectKeyIdentifier=hash
+authorityKeyIdentifier = keyid,issuer
+extendedKeyUsage = serverAuth
+keyUsage = Digital Signature, Key Encipherment
+subjectAltName = @alt_names
+[alt_names]
+DNS.1 = localhost" >> "$tmp"
 
 #create CA
 openssl ecparam -name prime256v1 -genkey -noout -out test/bdd/fixtures/keys/tls/ec-cakey.pem
@@ -20,8 +27,8 @@ openssl req -new -x509 -key test/bdd/fixtures/keys/tls/ec-cakey.pem -subj "/C=CA
 
 #create TLS creds
 openssl ecparam -name prime256v1 -genkey -noout -out test/bdd/fixtures/keys/tls/ec-key.pem
-openssl req -new -key test/bdd/fixtures/keys/tls/ec-key.pem -subj "/C=CA/ST=ON/O=Example Inc.:edge-agent/OU=edge-agent/CN=localhost" -reqexts SAN -out test/bdd/fixtures/keys/tls/ec-key.csr
-openssl x509 -req -in test/bdd/fixtures/keys/tls/ec-key.csr -extensions SAN -CA test/bdd/fixtures/keys/tls/ec-cacert.pem -CAkey test/bdd/fixtures/keys/tls/ec-cakey.pem -CAcreateserial -out test/bdd/fixtures/keys/tls/ec-pubCert.pem -days 365
+openssl req -new -key test/bdd/fixtures/keys/tls/ec-key.pem -subj "/C=CA/ST=ON/O=Example Inc.:edge-agent/OU=edge-agent/CN=localhost" -out test/bdd/fixtures/keys/tls/ec-key.csr
+openssl x509 -req -in test/bdd/fixtures/keys/tls/ec-key.csr -CA test/bdd/fixtures/keys/tls/ec-cacert.pem -CAkey test/bdd/fixtures/keys/tls/ec-cakey.pem -CAcreateserial -extfile "$tmp" -out test/bdd/fixtures/keys/tls/ec-pubCert.pem -days 365
 
 
 echo "done generating edge-agent PKI"
