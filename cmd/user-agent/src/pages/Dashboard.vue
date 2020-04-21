825293bf7943927c7935079ee9e4e99977ffb687
talwinder.kaur <talwinder.kaur@securekey.com>
2020-04-21 15:27:56 -0400

    feat: Dynamic Verifiable Credential Dashboard
    closes #89
    
    Signed-off-by: talwinder.kaur <talwinder.kaur@securekey.com>

--- cmd/user-agent/src/pages/Dashboard.vue
+++ cmd/user-agent/src/pages/Dashboard.vue
@@ -8,8 +8,7 @@ SPDX-License-Identifier: Apache-2.0
   <div class="content">
     <div class="md-layout">
 
-      <div
-        class="md-layout-item md-medium-size-50 md-xsmall-size-100 md-size-25">
+      <div class="md-layout-item md-medium-size-50 md-xsmall-size-100 md-size-25">
         <stats-card data-background-color="green">
           <template slot="header">
             <md-icon>store</md-icon>
@@ -22,8 +21,7 @@ SPDX-License-Identifier: Apache-2.0
           </template>
         </stats-card>
       </div>
-      <div
-        class="md-layout-item md-medium-size-50 md-xsmall-size-100 md-size-25">
+      <div class="md-layout-item md-medium-size-50 md-xsmall-size-100 md-size-25">
         <stats-card data-background-color="orange">
           <template slot="header">
             <md-icon>border_outer</md-icon>
@@ -37,8 +35,7 @@ SPDX-License-Identifier: Apache-2.0
           </template>
         </stats-card>
       </div>
-      <div
-              class="md-layout-item md-medium-size-50 md-xsmall-size-100 md-size-25">
+      <div class="md-layout-item md-medium-size-50 md-xsmall-size-100 md-size-25">
         <stats-card data-background-color="blue">
           <template slot="header">
             <md-icon>account_balance</md-icon>
@@ -52,8 +49,7 @@ SPDX-License-Identifier: Apache-2.0
           </template>
         </stats-card>
       </div>
-      <div
-              class="md-layout-item md-medium-size-50 md-xsmall-size-100 md-size-25">
+      <div class="md-layout-item md-medium-size-50 md-xsmall-size-100 md-size-25">
         <stats-card data-background-color="purple">
           <template slot="header">
             <md-icon>description</md-icon>
@@ -70,15 +66,17 @@ SPDX-License-Identifier: Apache-2.0
     </div>
     <div class="content">
       <div class="md-layout">
-        <div
-                class="md-layout-item md-medium-size-100 md-xsmall-size-100 md-size-100"
-        >
+        <div class="md-layout-item md-medium-size-100 md-xsmall-size-100 md-size-100">
           <md-card>
             <md-card-header data-background-color="green">
               <h4 class="title"> <md-icon>content_paste</md-icon> My Stored Credentials</h4>
+              <p class="category"> If you have successfully saved credentials, but not able to view table below. Wait 9 to 10 sec and switch to any other page and comeback</p>
             </md-card-header>
             <md-card-content>
-              <simple-table table-header-color="green"></simple-table>
+              <simple-table v-for="vc in verifiableCredential" v-bind:key=vc.name
+                            v-bind:name="vc.name"
+                            v-bind:credential="vc.credential">
+              </simple-table>
             </md-card-content>
           </md-card>
         </div>
@@ -89,19 +87,54 @@ SPDX-License-Identifier: Apache-2.0
 </template>
 
 <script>
-import {StatsCard} from "@/components";
-import {SimpleTable} from "@/components";
-
-export default {
-  components: {
-    StatsCard,
-    SimpleTable,
-  },
-  data() {
-    return {
+  import {StatsCard} from "@/components";
+  import {SimpleTable} from "@/components";
+  let vcData = [];
+  async function fetchCredentials() {
+      // Get the VC data
+    for (let i = 0; i < vcData.length; i++) {
+      await window.$aries.verifiable.getCredential({
+        id: vcData[i].id
+      }).then(resp => {
+        vcData[i].credential= JSON.parse(resp.verifiableCredential)
       }
+      ).catch(err =>
+              console.log('get vc failed : errMsg=' + err)
+      )
+    }
+  }
+
+  export default {
+    components: {
+      StatsCard,
+      SimpleTable,
     },
-  };
+    beforeCreate: async function () {
+      // Load the Credentials
+      let aries = await this.$arieslib
+      await aries.verifiable.getCredentials()
+              .then(resp => {
+                        vcData = resp.result
+                        if (vcData && vcData.length == 0) {
+                          console.log('no credentials exists')
+                        }
+                        console.log('all data are :' + JSON.stringify(vcData))
+                      }
+              ).catch(err => {
+                        console.log('get credentials failed : errMsg=' + err)
+                      }
+              )
+
+      window.$webCredentialHandler = this.$webCredentialHandler
+      window.$aries = aries
+      await fetchCredentials()
+    },
+    data() {
+      return {
+        verifiableCredential: vcData
+      }
+    }
+  }
 </script>
 
 <style>
@@ -109,3 +142,4 @@ export default {
     text-transform: capitalize;
   }
 </style>
+
