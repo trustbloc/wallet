53429a732df80bb80b1a8e5e9ae9fab45091ec98
Firas Qutishat <firas.qutishat@securekey.com>
2020-05-04 15:32:46 -0400

    fix: remove gunzip for wasm
    Signed-off-by: Firas Qutishat <firas.qutishat@securekey.com>

--- cmd/user-agent/scripts/setup_assets.sh
+++ cmd/user-agent/scripts/setup_assets.sh
@@ -9,11 +9,9 @@ rm -rf public/aries-framework-go/
 mkdir -p public/aries-framework-go/assets
 npm install
 cp -Rp node_modules/@hyperledger/aries-framework-go/dist/assets/* public/aries-framework-go/assets
-gunzip public/aries-framework-go/assets/aries-js-worker.wasm.gz
 
 
 rm -rf public/trustbloc-agent/
 mkdir -p public/trustbloc-agent/assets
 npm install
 cp -Rp node_modules/@trustbloc/trustbloc-agent/dist/assets/* public/trustbloc-agent/assets
-gunzip public/trustbloc-agent/assets/trustbloc-agent-js-worker.wasm.gz
