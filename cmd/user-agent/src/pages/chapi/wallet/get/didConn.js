d76cffee7205ff98c938c2034bce10a5b227e404
sudesh.shetty <sudesh.shetty@securekey.com>
2020-06-05 17:01:19 -0400

    feat: webwallet did connection credential response as VP
    - Closes # #195
    
    Signed-off-by: sudesh.shetty <sudesh.shetty@securekey.com>

--- cmd/user-agent/src/pages/chapi/wallet/get/didConn.js
+++ cmd/user-agent/src/pages/chapi/wallet/get/didConn.js
@@ -18,7 +18,9 @@ export class DIDConn {
         this.walletManager = new WalletManager()
         this.credEvent = credEvent
 
-        const {invitation} = getInvitation(credEvent);
+        const {domain, challenge, invitation} = getRequestParams(credEvent);
+        this.domain = domain
+        this.challenge = challenge
         this.invitation = invitation
     }
 
@@ -58,8 +60,61 @@ export class DIDConn {
         await this.walletManager.storeWalletMetadata(this.walletUser.id, this.walletUser)
 
 
-        // TODO response to be signed
-        this.sendResponse("VerifiablePresentation", didConnResponse(this.walletUser.did, connection.result))
+        let responseData = await this._didConnResponse(connection.result)
+        this.sendResponse("VerifiablePresentation", responseData)
+    }
+
+
+    async _didConnResponse(connection) {
+        // TODO add proof to DIDConnection [Issue #194]
+        let presentation = {
+            "@context": [
+                "https://www.w3.org/2018/credentials/v1"
+            ],
+            type: "VerifiablePresentation",
+            holder: this.walletUser.did,
+            verifiableCredential: {
+                "@context": [
+                    "https://www.w3.org/2018/credentials/v1",
+                    "https://trustbloc.github.io/context/vc/examples-ext-v1.jsonld"
+                ],
+                type: [
+                    "VerifiableCredential",
+                    "DIDConnection"
+                ],
+                credentialSubject: {
+                    id: connection.ConnectionID,
+                    threadID: connection.ThreadID,
+                    inviteeDID: connection.MyDID,
+                    inviterDID: connection.TheirDID,
+                    inviterLabel: connection.TheirLabel,
+                    connectionState: connection.State,
+                }
+            }
+        }
+
+        let data
+        await this.aries.verifiable.generatePresentation({
+            presentation: presentation,
+            domain: this.domain,
+            challenge: this.challenge,
+            did: this.walletUser.did,
+            signatureType: this.walletUser.signatureType,
+            skipVerify: true,
+        }).then(resp => {
+                if (!resp.verifiablePresentation) {
+                    data = "failed to create did connection presentation"
+                    return
+                }
+
+                data = resp.verifiablePresentation
+            }
+        ).catch(err => {
+            data = err
+            console.log('failed to create presentation, errMsg:', err)
+        })
+
+        return data
     }
 
     async waitFor(connectionID, states, callback, timeout) {
@@ -110,35 +165,26 @@ export class DIDConn {
     }
 }
 
-function getInvitation(credEvent) {
+function getRequestParams(credEvent) {
     if (!credEvent.credentialRequestOptions.web.VerifiablePresentation) {
         throw "invitation not found in did connect credential event"
     }
 
-    return credEvent.credentialRequestOptions.web.VerifiablePresentation
-}
+    const verifiable = credEvent.credentialRequestOptions.web.VerifiablePresentation
 
-function didConnResponse(holder, connection) {
-    return {
-        "@context": "https://www.w3.org/2018/credentials/v1",
-        type: "VerifiablePresentation",
-        holder: holder,
-        verifiableCredential: {
-            "@context": [
-                "https://www.w3.org/2018/credentials/v1"
-            ],
-            type: [
-                "VerifiableCredential",
-                "DIDConnection"
-            ],
-            credentialSubject: {
-                id: connection.ConnectionID,
-                threadID: connection.ThreadID,
-                inviteeDID: connection.MyDID,
-                inviterDID: connection.TheirDID,
-                inviterLabel: connection.TheirLabel,
-                state: connection.State,
-            }
-        }
+    let {challenge, domain, query, invitation} = verifiable;
+
+    if (query && query.challenge) {
+        challenge = query.challenge;
     }
+
+    if (query && query.domain) {
+        domain = query.domain;
+    }
+
+    if (!domain && credEvent.credentialRequestOrigin) {
+        domain = credEvent.credentialRequestOrigin.split('//').pop()
+    }
+
+    return {domain, challenge, invitation}
 }
