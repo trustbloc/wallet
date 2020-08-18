69a9562a462036b60542fbf46b7dfb3cef3b90f4
Filip Burlacu <filip.burlacu@securekey.com>
2020-08-18 18:41:33 -0400

    chore: update trustbloc-did-method dependency
    Signed-off-by: Filip Burlacu <filip.burlacu@securekey.com>

--- scripts/user_agent_start.sh
+++ scripts/user_agent_start.sh
@@ -9,4 +9,9 @@ set -e
 echo "Running $0"
 
 cd test/bdd/fixtures/agent-wasm
-(source .env && docker-compose down && docker-compose up --force-recreate)
+
+(source .env && docker-compose down --remove-orphans && docker-compose -f docker-compose.yml up --force-recreate -d)
+
+bash generate_config.sh
+
+(source .env && docker-compose -f discovery-compose.yml up --force-recreate -d)
