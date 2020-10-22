c1a38bf09cb9a86e450d2cd6d25ad04aea4fede1
talwinder.kaur <talwinder.kaur@securekey.com>
2020-10-22 08:36:47 -0400

    feat: Implement device login handler
    closes #432
    
    Signed-off-by: talwinder.kaur <talwinder.kaur@securekey.com>

--- pkg/restapi/device/deviceapi.go
+++ pkg/restapi/device/deviceapi.go
@@ -13,36 +13,36 @@ import (
 
 // Device represents the device model.
 type Device struct {
-	id          string
-	name        string
-	displayName string
-	credentials []webauthn.Credential
+	ID          string
+	Name        string
+	DisplayName string
+	Credentials []webauthn.Credential
 }
 
 // NewDevice creates and returns a new Device.
 func NewDevice(usr *user.User) *Device {
 	device := &Device{}
-	device.id = usr.Sub
-	device.name = usr.Name
-	device.displayName = usr.FamilyName
-	device.credentials = []webauthn.Credential{}
+	device.ID = usr.Sub
+	device.Name = usr.Name
+	device.DisplayName = usr.FamilyName
+	device.Credentials = []webauthn.Credential{}
 
 	return device
 }
 
 // WebAuthnID returns the user's ID.
 func (d Device) WebAuthnID() []byte {
-	return []byte(d.id)
+	return []byte(d.ID)
 }
 
 // WebAuthnName returns the user's username.
 func (d Device) WebAuthnName() string {
-	return d.name
+	return d.Name
 }
 
-// WebAuthnDisplayName returns the user's display name.
+// WebAuthnDisplayName returns the user's display Name.
 func (d Device) WebAuthnDisplayName() string {
-	return d.displayName
+	return d.DisplayName
 }
 
 // WebAuthnIcon is not (yet) implemented.
@@ -53,20 +53,20 @@ func (d Device) WebAuthnIcon() string {
 // AddCredential associates the credential to the user.
 // nolint:gocritic
 func (d *Device) AddCredential(cred webauthn.Credential) {
-	d.credentials = append(d.credentials, cred)
+	d.Credentials = append(d.Credentials, cred)
 }
 
-// WebAuthnCredentials returns credentials owned by the user.
+// WebAuthnCredentials returns Credentials owned by the user.
 func (d Device) WebAuthnCredentials() []webauthn.Credential {
-	return d.credentials
+	return d.Credentials
 }
 
 // CredentialExcludeList returns a CredentialDescriptor array filled.
-// with all the user's credentials.
+// with all the user's Credentials.
 func (d Device) CredentialExcludeList() []protocol.CredentialDescriptor {
 	credentialExcludeList := []protocol.CredentialDescriptor{}
 
-	for _, cred := range d.credentials {
+	for _, cred := range d.Credentials {
 		descriptor := protocol.CredentialDescriptor{
 			Type:         protocol.PublicKeyCredentialType,
 			CredentialID: cred.ID,
