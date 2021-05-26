b594fc3afa22ab61ea12c0c00e1f1cc53eadb030
talwinder.kaur <talwinder.kaur@securekey.com>
2021-05-25 21:34:21 -0400

    fix: Enable the beat loader in edge-agent login
    closes #737
    
    Signed-off-by: talwinder.kaur <talwinder.kaur@securekey.com>

--- cmd/wallet-web/src/pages/chapi/Login.vue
+++ cmd/wallet-web/src/pages/chapi/Login.vue
@@ -76,7 +76,7 @@ SPDX-License-Identifier: Apache-2.0
       <footer class="footer footer-gradient relative py-24  border-b-2 border-blue-700">
           <div class="container mx-auto px-6 border-gray-300 flex flex-col items-center">
             <div class="sm:w-2/3 py-16">
-              <p class="text-sm text-white text-center font-semibold  mb-2">
+              <p class="text-sm text-white text-center font-light  mb-2">
                 Copyright &copy; <a href="https://securekey.com/">SecureKey Technologies</a> and the TrustBloc Contributors.
               </p>
             </div>
@@ -89,7 +89,7 @@ SPDX-License-Identifier: Apache-2.0
 <script>
     import {DeviceLogin, RegisterWallet} from "./wallet"
     import {mapActions, mapGetters} from 'vuex'
-    import BeatLoader from "@saeris/vue-spinners";
+    import {BeatLoader} from "@saeris/vue-spinners";
 
     export default {
         created: async function () {
