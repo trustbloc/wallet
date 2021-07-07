1abbfe313d5b69c1b140cb217699b0f595c0b3cc
talwinder kaur <talwinder.kaur@securekey.com>
2021-07-07 03:02:32 -0400

    feat: Add dynamic footer year and reshuffle footer alignment for small screen
    Signed-off-by: talwinder kaur <talwinder.kaur@securekey.com>

--- cmd/wallet-web/src/pages/layout/ContentFooter.vue
+++ cmd/wallet-web/src/pages/layout/ContentFooter.vue
@@ -5,18 +5,34 @@ SPDX-License-Identifier: Apache-2.0
 */
 
 <template>
-  <div class="fixed bottom-0 mx-16 mb-4 lg:w-full sm:w-xl lg:mx-0 xl:mx-0 2xl:mx-0 text-center">
-    <div class="text-neutrals-medium relative inline-block">
-      © 2021 TrustBloc ･ Privacy Policy ･ Terms of Service  ･
+  <div class="fixed bottom-0 mx-auto px-18 mb-4 w-full sm:px-20 xs:px-20 lg:mx-0 xl:mx-0 2xl:mx-0 text-center">
+    <div class="text-neutrals-medium mx-auto 2xl:block xl:block lg:block md:block hidden  text-center">
+       © {{ date }} TrustBloc ･ Privacy Policy ･ Terms of Service  ･
       <select class="py-4" style="background-color: transparent">
         <option class="text-neutrals-dark block  text-sm"  id="english">English</option>
         <option class="text-neutrals-medium block  text-sm"  id="french">Français</option>
       </select>
+    </div>
+    <div class="text-neutrals-medium relative 2xl:hidden xl:hidden lg:hidden md:hidden block">
+      <ul class="list-inside">
+        <li> Privacy Policy ･ Terms of Service</li>
+        <li>© {{ date }} TrustBloc ･
+          <select class="py-4" style="background-color: transparent">
+            <option class="text-neutrals-dark block  text-sm">English</option>
+            <option class="text-neutrals-medium block  text-sm">Français</option>
+          </select>
+        </li>
+      </ul>
     </div>
   </div>
 </template>
 <script>
 export default {
   name: "footer",
+  data() {
+    return {
+      date: new Date().getFullYear(),
+    };
+  },
 };
 </script>
