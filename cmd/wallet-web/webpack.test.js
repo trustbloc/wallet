8b594c3247115c7269ba59cfb2dde84bee70f22a
sudesh.shetty <sudesh.shetty@securekey.com>
2021-07-29 16:59:23 -0400

    chore: enable wallet web unit test in CI
    Signed-off-by: sudesh.shetty <sudesh.shetty@securekey.com>

--- cmd/wallet-web/webpack.test.js
+++ cmd/wallet-web/webpack.test.js
@@ -4,38 +4,32 @@ Copyright SecureKey Technologies Inc. All Rights Reserved.
 SPDX-License-Identifier: Apache-2.0
 */
 
-const VueLoaderPlugin = require('vue-loader/lib/plugin')
+const VueLoaderPlugin = require('vue-loader/lib/plugin');
 
-const {alias} = require('./alias.config')
+const { alias } = require('./alias.config');
 
 module.exports = {
-    mode: 'development',
-    module: {
-        rules: [
-            {
-                test: /\.vue$/,
-                loader: 'vue-loader'
-            },
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                loader: 'babel-loader'
-            },
-            {
-                test: /\.css$/,
-                use: [
-                    'vue-style-loader',
-                    'css-loader'
-                ]
-            }
-        ]
-    },
-    resolve: {
-        alias
-    },
-    plugins: [
-        new VueLoaderPlugin()
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader',
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+      },
+      {
+        test: /\.css$/,
+        use: ['vue-style-loader', 'css-loader'],
+      },
     ],
-    devtool: 'inline-cheap-module-source-map'
-}
-
+  },
+  resolve: {
+    alias,
+  },
+  plugins: [new VueLoaderPlugin()],
+  devtool: 'inline-cheap-module-source-map',
+};
