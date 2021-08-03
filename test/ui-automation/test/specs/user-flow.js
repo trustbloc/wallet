74baac2b14e8c73755884b7e078dd88f7246bbb2
Rolson Quadras <rolson.quadras@securekey.com>
2021-08-03 10:42:01 -0400

    test: basic user flow test
    copied from https://github.com/trustbloc/sandbox/pull/1149/files
    
    Signed-off-by: Rolson Quadras <rolson.quadras@securekey.com>

---
+++ test/ui-automation/test/specs/user-flow.js
@@ -0,0 +1,65 @@
+/*
+Copyright SecureKey Technologies Inc. All Rights Reserved.
+
+SPDX-License-Identifier: Apache-2.0
+*/
+
+'use strict';
+
+const {wallet} = require('../helpers');
+
+describe("TrustBloc Wallet - SignUp and SignIn flow", () => {
+    const ctx = {
+        email: `ui-aut-${new Date().getTime()}@test.com`,
+    };
+
+    // runs once before the first test in this block
+    before(async () => {
+        await browser.reloadSession();
+        await browser.maximizeWindow();
+    });
+
+    beforeEach(function () {
+    });
+
+    it(`User Sign up (${ctx.email})`, async function () {
+        this.timeout(300000);
+
+        // 1. Navigate to Wallet Website
+        await browser.navigateTo(browser.config.walletURL);
+
+        // 2. Initialize Wallet (register/sign-up/etc.)
+        await wallet.init(ctx);
+    });
+
+    it(`User Logout (${ctx.email})`, async function () {
+        this.timeout(300000);
+
+        // 1. Navigate to Wallet Website
+        await browser.navigateTo(browser.config.walletURL);
+
+        // 2. Initialize Wallet (register/sign-up/etc.)
+        await wallet.logout(ctx);
+    });
+
+    it(`User Sign in (${ctx.email})`, async function () {
+        this.timeout(300000);
+
+        // 1. Navigate to Wallet Website
+        await browser.navigateTo(browser.config.walletURL);
+
+        // 2. Sign In to the registered Wallet (register/sign-up/etc.)
+        await wallet.signIn(ctx);
+    });
+
+    it(`User Logout (${ctx.email})`, async function () {
+        this.timeout(300000);
+
+        // 1. Navigate to Wallet Website
+        await browser.navigateTo(browser.config.walletURL);
+
+        // 2. Initialize Wallet (register/sign-up/etc.)
+        await wallet.logout(ctx);
+    });
+})
+
--- test/ui-automation/wdio.shared.conf.js
+++ test/ui-automation/wdio.shared.conf.js
@@ -32,7 +32,7 @@ exports.config = {
     // directory is where your package.json resides, so `wdio` will be called from there.
     //
     specs: [
-        './test/specs/signup.js'
+        './test/specs/user-flow.js'
     ],
     // Patterns to exclude.
     exclude: [
