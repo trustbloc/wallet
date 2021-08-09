98ebc0878ad4d54db893b78a2bdd774f725255dc
Anton Biriukov <anton.biriukov@securekey.com>
2021-08-09 14:18:03 -0400

    refactor: split translations into separate modules for each component
    Signed-off-by: Anton Biriukov <anton.biriukov@securekey.com>

---
+++ cmd/wallet-web/src/translations/fr.js
@@ -0,0 +1,11 @@
+import App from './App/fr.json';
+import ContentFooter from './ContentFooter/fr.json';
+import Signin from './Signin/fr.json';
+import Signup from './Signup/fr.json';
+
+export default {
+  App,
+  ContentFooter,
+  Signin,
+  Signup,
+};
