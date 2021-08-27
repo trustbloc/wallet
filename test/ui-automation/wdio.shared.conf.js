aeddf973715b4a65b3654df099285fbba37303c9
Rolson Quadras <rolson.quadras@securekey.com>
2021-08-27 13:10:03 -0400

    test: [ui-automation] chapi credential store/get
    Signed-off-by: Rolson Quadras <rolson.quadras@securekey.com>

--- test/ui-automation/wdio.shared.conf.js
+++ test/ui-automation/wdio.shared.conf.js
@@ -32,7 +32,7 @@ exports.config = {
     // directory is where your package.json resides, so `wdio` will be called from there.
     //
     specs: [
-        './test/specs/user-flow.js'
+        './test/specs/credential-interaction-flow.js'
     ],
     // Patterns to exclude.
     exclude: [
