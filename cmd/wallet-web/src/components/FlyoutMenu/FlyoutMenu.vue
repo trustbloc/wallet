7a1fd51d5124b68d9884cc398606f4ea78c1ea65
talwinder50 <talwinderkaur50@gmail.com>
2021-10-08 23:48:13 -0400

    feat: Add fly out menu list and tooltip for flyout menu
    closes #1152
    
    Signed-off-by: talwinder50 <talwinderkaur50@gmail.com>

--- cmd/wallet-web/src/components/FlyoutMenu/FlyoutMenu.vue
+++ cmd/wallet-web/src/components/FlyoutMenu/FlyoutMenu.vue
@@ -1,32 +1,81 @@
 <template>
-  <div>
+  <div class="h-11">
     <!--Todo make flyout icon and text configurable -->
-    <button v-if="type === 'default'" class="inline-flex items-center py-2 px-3 w-screen md:w-auto">
+    <button
+      v-if="type === 'default'"
+      id="default"
+      class="inline-flex items-center py-2 px-3 w-screen md:w-auto"
+    >
       <div class="flex-none w-6 h-6">
         <img src="@/assets/img/icons-sm--vault-icon.svg" />
       </div>
       <div class="flex-grow pl-2 text-left">
-        <span class="text-sm font-bold text-neutrals-dark">All Vaults</span>
+        <span class="text-sm font-bold text-neutrals-dark">{{ i18n.allVaultLabel }}</span>
       </div>
       <div class="flex-none w-6 h-6">
         <img src="@/assets/img/icons-sm--chevron-down-icon.svg" />
       </div>
     </button>
-    <button v-if="type === 'outline'" class="py-2 px-3 w-11 h-11">
-      <div class="flex-none">
-        <img src="@/assets/img/more-icon.svg" />
+    <button
+      v-if="type === 'outline'"
+      id="outline"
+      class="
+        w-11
+        h-11
+        bg-neutrals-white
+        rounded-lg
+        focus:border-neutrals-chatelle
+        border-opacity-10
+        focus:ring-primary-purple focus:ring-opacity-70 focus:ring-2
+        focus-within:ring-offset-2
+        border border-neutrals-chatelle
+        hover:border-neutrals-mountainMist-light
+      "
+      @mouseover="showTooltip = !showTooltip"
+      @focus="showTooltip = !showTooltip"
+    >
+      <div class="flex-none p-2">
+        <img src="@/assets/img/more-icon.svg" @click="showFlyoutMenuList = !showFlyoutMenuList" />
+      </div>
+      <div v-if="showTooltip" id="tooltip">
+        <tool-tip :tool-tip-label="i18n.toolTipLabel"></tool-tip>
       </div>
     </button>
+    <div v-if="showFlyoutMenuList" id="flyoutMenuList" class="relative">
+      <flyout-menu-list />
+    </div>
   </div>
 </template>
 
 <script>
+import ToolTip from '@/components/ToolTip/ToolTip.vue';
+import FlyoutMenuList from '@/components/FlyoutMenu/FlyoutMenuList.vue';
+
 export default {
   name: 'FlyoutMenu',
+  components: {
+    ToolTip,
+    FlyoutMenuList,
+  },
   props: {
     type: {
       type: String,
       default: 'default',
+    },
+  },
+  data() {
+    return {
+      toolTipLabel: {
+        type: String,
+        default: 'default',
+      },
+      showTooltip: false,
+      showFlyoutMenuList: false,
+    };
+  },
+  computed: {
+    i18n() {
+      return this.$t('CredentialDetails');
     },
   },
 };
---
+++ cmd/wallet-web/src/components/FlyoutMenu/FlyoutMenuList.vue
@@ -0,0 +1,37 @@
+<template>
+  <div
+    class="
+      overflow-hidden
+      absolute
+      right-0
+      z-20
+      mt-2
+      w-56
+      max-w-max
+      text-neutrals-medium
+      bg-neutrals-white
+      rounded-lg
+      shadow-xl
+      bg-white
+    "
+  >
+    <div class="p-4 text-base text-start">
+      <button class="block pb-2 font-bold">{{ i18n.renameCredential }}</button>
+      <button class="block pb-2 font-bold">{{ i18n.moveCredential }}</button>
+      <button class="block font-bold text-primary-vampire">{{ i18n.deleteCredential }}</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'FlyoutMenuList',
+  computed: {
+    i18n() {
+      return this.$t('CredentialDetails');
+    },
+  },
+};
+</script>
+
+<style scoped></style>
