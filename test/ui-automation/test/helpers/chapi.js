acc40d2cc2fa35d11a972d3885e9b98896a3e256
Anton Biriukov <anton.biriukov@securekey.com>
2022-01-19 22:54:34 +0400

    fix: adjusted chapi automation test
    Signed-off-by: Anton Biriukov <anton.biriukov@securekey.com>

--- test/ui-automation/test/helpers/chapi.js
+++ test/ui-automation/test/helpers/chapi.js
@@ -7,47 +7,50 @@ The license details are available at https://github.com/w3c-ccg/chapi-interop-te
 SPDX-License-Identifier: Apache-2.0
 */
 
-'use strict';
+"use strict";
 
-exports.chooseWallet = async ({name}) => {
-    const chapiFrame = await $('iframe');
-    await chapiFrame.waitForExist();
-    await browser.switchToFrame(chapiFrame);
+exports.chooseWallet = async ({ name }) => {
+  const chapiFrame = await $("iframe");
+  await chapiFrame.waitForExist();
+  await browser.switchToFrame(chapiFrame);
 
-    const nextButton = await $('button*=Next');
-    await nextButton.waitForClickable();
-    // FIXME: some issue with next button causes this to fail about 20%
-    await browser.pause(1000);
-    await nextButton.click();
+  const nextButton = await $("button*=Next");
+  await nextButton.waitForClickable();
+  // FIXME: some issue with next button causes this to fail about 20%
+  await browser.pause(1000);
+  await nextButton.click();
 
-    const demoWallet = await $(`strong*=${name}`);
-    await demoWallet.waitForClickable();
-    await demoWallet.click();
+  const rememberChoiceBtn = await $("span*=Remember");
+  await rememberChoiceBtn.waitForClickable();
+  await rememberChoiceBtn.click();
 
-    const innerWalletFrame = await $('iframe');
-    await innerWalletFrame.waitForExist();
-    await browser.switchToFrame(innerWalletFrame);
+  const demoWallet = await $(`strong*=${name}`);
+  await demoWallet.waitForClickable();
+  await demoWallet.click();
 
-    let dialogs;
-    await browser.waitUntil(async () => {
-        dialogs = await $$('dialog');
-        return dialogs.length === 2;
-    });
+  const innerWalletFrame = await $("iframe");
+  await innerWalletFrame.waitForExist();
+  await browser.switchToFrame(innerWalletFrame);
 
-    const innerWalletFrame2 = await dialogs[1].$('iframe');
-    await innerWalletFrame2.waitForExist();
-    await browser.switchToFrame(innerWalletFrame2);
+  let dialogs;
+  await browser.waitUntil(async () => {
+    dialogs = await $$("dialog");
+    return dialogs.length === 2;
+  });
+
+  const innerWalletFrame2 = await dialogs[1].$("iframe");
+  await innerWalletFrame2.waitForExist();
+  await browser.switchToFrame(innerWalletFrame2);
 };
 
-
 exports.allow = async () => {
-    const chapiFrame = await $('iframe');
-    await chapiFrame.waitForExist();
-    expect(chapiFrame).toHaveAttrContaining('src', 'https://authn.io/mediator');
-    await browser.switchToFrame(chapiFrame);
-    const allowBtn = await $('button*=Allow');
-    await allowBtn.waitForClickable();
-    await allowBtn.click();
-    await chapiFrame.waitForExist({reverse: true});
-    await browser.switchToFrame(null);
+  const chapiFrame = await $("iframe");
+  await chapiFrame.waitForExist();
+  expect(chapiFrame).toHaveAttrContaining("src", "https://authn.io/mediator");
+  await browser.switchToFrame(chapiFrame);
+  const allowBtn = await $("button*=Allow");
+  await allowBtn.waitForClickable();
+  await allowBtn.click();
+  await chapiFrame.waitForExist({ reverse: true });
+  await browser.switchToFrame(null);
 };
