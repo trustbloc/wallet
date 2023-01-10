9b4758b99e77cccaf2ff66bf80b37891ddda276f
Anton Biriukov <anton.biriukov@avast.com>
2023-01-10 03:43:05 -0500

    feat: implemented UI pages for openid4ci flow
    Signed-off-by: Anton Biriukov <anton.biriukov@avast.com>

---
+++ cmd/wallet-web/src/layouts/OpenID4VCSaveLayout.vue
@@ -0,0 +1,30 @@
+<!--
+ * Copyright SecureKey Technologies Inc. All Rights Reserved.
+ * Copyright Avast Software. All Rights Reserved.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+-->
+
+<script>
+import { reactive } from 'vue';
+import OpenID4VCSavePage from '@/pages/OpenID4VCSavePage.vue';
+
+export const OpenID4VCSaveLayoutStore = reactive({
+  component: OpenID4VCSavePage,
+});
+</script>
+
+<script setup>
+import { ref } from 'vue';
+
+const component = ref(OpenID4VCSaveLayoutStore.component);
+</script>
+
+<template>
+  <keep-alive>
+    <component
+      :is="component"
+      class="relative z-10 flex h-full w-full grow flex-col items-start justify-start overflow-hidden bg-neutrals-softWhite"
+    />
+  </keep-alive>
+</template>
